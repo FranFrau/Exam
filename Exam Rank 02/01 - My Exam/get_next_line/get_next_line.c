48bf1b1fe688a29dae54fd5e14b6a6c37a4ba060
Francesco Frau <ffrau@e3r9p1.42roma.it>
2022-02-26 12:58:09 +0100

    Exam Rank 02 update - Bug fixes - Added my exam exercises - Fixed a distraction issue in get_next_line

--- Exam Rank 02/01 - My Exam/get_next_line/get_next_line.c
+++ Exam Rank 02/01 - My Exam/get_next_line/get_next_line.c
@@ -1,46 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gnl.c                                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ffrau <ffrau@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ffrau <ffrau@student.42roma.it>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/10 10:41:38 by ffrau             #+#    #+#             */
-/*   Updated: 2022/02/10 10:41:48 by ffrau            ###   ########.fr       */
+/*   Created: 2022/02/23 14:26:31 by ffrau             #+#    #+#             */
+/*   Updated: 2022/02/23 15:37:04 by ffrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <strings.h>
-# include <fcntl.h>
-# include <stdio.h>
-# define BUFFER_SIZE 5
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
 	char	a[999999];
 	char	buffer[1];
 	char	*new_a;
-	int		i;
- 
-	if (fd < 0 && BUFFER_SIZE <= 0)
+	int		i = 0;
+
+	buffer[0] = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	i = 0;
-	a[i] = 0;
+	a[0] = 0;
 	while (read(fd, buffer, 1) == 1)
 	{
 		a[i] = buffer[0];
 		a[i + 1] = '\0';
-		if (a[i] == '\n')
-			break;
+		if (a[i] == '\n' || a[i] == '\0')
+			break ;
 		i++;
+		buffer[0] = 0;
 	}
 	if (!a[0])
 		return (NULL);
 	new_a = malloc(i + 1);
-	if (!new_a)
-		return (NULL);
 	i = 0;
 	while (a[i])
 	{
@@ -49,21 +43,4 @@ char	*get_next_line(int fd)
 	}
 	new_a[i] = '\0';
 	return (new_a);
-}
-
-int main(int ac, char **av){
-	//Thx to mprigent for the main
-	int		fd;
-	char	*line;
-	(void)ac;
-
-	fd = open(av[1], O_RDONLY);
-    while ((line = get_next_line(fd)) != 0)
-	{
-		printf("%s", line);
-		free(line);
-	}
-	close(fd);
-	system("leaks a.out");
-	return (0);
 }
