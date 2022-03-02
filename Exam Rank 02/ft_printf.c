edf3c97a03b5369addf0d5d3d6018472e93fbea7
FranFrau <36134040+FranFrau@users.noreply.github.com>
2022-03-02 12:58:49 +0100

    Updated ft_printf

--- Exam Rank 02/ft_printf.c
+++ Exam Rank 02/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ffrau <ffrau@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ffrau <ffrau@student.42roma.it>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/14 18:29:16 by ffrau             #+#    #+#             */
-/*   Updated: 2022/02/14 18:29:34 by ffrau            ###   ########.fr       */
+/*   Created: 2022/03/01 15:34:13 by ffrau             #+#    #+#             */
+/*   Updated: 2022/03/02 12:58:33 by ffrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,6 @@
 int	ft_putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-
-int	nb_len(long nb, int nb_base)
-{
-	int	count;
-
-	count = 0;
-	if (nb < 0)
-	{
-		count++;
-		nb *= -1;
-	}
-	while (nb > 0)
-	{
-		nb /= nb_base;
-		count++;
-	}
-	return (count);
-}
-
-int	ft_atoi(int nb, char *base, int nb_base)
-{
-	if (nb == -2147483648)
-	{
-		ft_atoi((nb / nb_base), base, nb_base);
-		ft_putchar('8');
-	}
-	else if (nb < 0)
-	{
-		if (nb_base != 16)
-			ft_putchar('-');
-		ft_atoi(nb * -1, base, nb_base);		
-	}
-	else
-	{
-		if (nb < 10)
-			ft_putchar('0' + nb);
-		else
-		{
-			ft_atoi(nb / nb_base, base, nb_base);
-			ft_putchar(base[nb % nb_base]);
-		}
-	}
-	return (nb_len(nb, nb_base));
 }
 
 int	ft_putstr(char *c)
@@ -74,19 +30,25 @@ int	ft_putstr(char *c)
 	return (count);
 }
 
-int	find_types(char c, va_list list)
+int	ft_atoi(long long nb, char *base, int nb_base)
 {
-	unsigned int	x;
-	char			*s;
-	int				d;
-	
-	if (c == 'd')
-		return (ft_atoi(va_arg(list, int), "0123456789", 10));
-	else if (c == 's')
-		return (ft_putstr(va_arg(list, char *)));
-	else if (c == 'x')
-		return (ft_atoi(va_arg(list, unsigned int), "0123456789abcdef", 16));
-	return (0);
+	int	count;
+
+	count = 0;
+	if (nb == -2147483648 && nb_base == 10)
+		return (ft_putstr("-2147483648"));
+	else if (nb < 0)
+	{
+		if (nb_base != 16)
+			count += ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb < 10)
+		return (count += ft_putchar('0' + nb));
+	if (nb / nb_base > 0)
+		count += ft_atoi(nb / nb_base, base, nb_base);
+	count += ft_putchar(base[nb % nb_base]);
+	return (count);
 }
 
 int	ft_printf(const char *s, ...)
@@ -104,8 +66,14 @@ int	ft_printf(const char *s, ...)
 	{
 		if (s[i] == '%')
 		{
-			count += find_types(s[i + 1], list);
-			i += 2;
+			i++;
+			if (s[i] == 'd')
+				count += ft_atoi(va_arg(list, int), "0123456789", 10);
+			else if (s[i] == 's')
+				count += ft_putstr(va_arg(list, char *)));
+			else if (s[i] == 'x')
+				count += ft_atoi(va_arg(list, unsigned int), "0123456789abcdef", 16);
+			i++;
 		}
 		if (!s[i])
 			return (count);
@@ -114,16 +82,16 @@ int	ft_printf(const char *s, ...)
 	}
 	return (count);
 }
-/*
-#include <stdio.h> 
-int main()
-{
-	printf(" - Count ft_printf: %d\n", ft_printf("Hello %s", "toto"));
-	printf(" - Count printf: %d\n", printf("Hello %s", "toto"));
-	printf(" - Count ft_printf: %d\n", ft_printf("%s %s", "I'm"));
-	printf(" - Count printf: %d\n", printf("%s %s", "I'm"));
-	printf(" - Count ft_printf: %d\n", ft_printf("Magic %s is %d", "number", 42));
-	printf(" - Count printf: %d\n", printf("Magic %s is %d", "number", 42));
-	printf(" - Count ft_printf: %d\n", ft_printf("Hexadecimal for %d is %x", 42, 589625));
-	printf(" - Count printf: %d\n", printf("Hexadecimal for %d is %x", 42, 589625));
-}*/
+
+// #include <stdio.h> 
+// int main()
+// {
+// 	printf(" - Count ft_printf: %d\n", ft_printf("Hello %s", "toto"));
+// 	printf(" - Count printf: %d\n", printf("Hello %s", "toto"));
+// 	printf(" - Count ft_printf: %d\n", ft_printf("%s %s", "I'm"));
+// 	printf(" - Count printf: %d\n", printf("%s %s", "I'm"));
+// 	printf(" - Count ft_printf: %d\n", ft_printf("Magic %s is %d", "number", 42));
+// 	printf(" - Count printf: %d\n", printf("Magic %s is %d", "number", 42));
+// 	printf(" - Count ft_printf: %d\n", ft_printf("Hexadecimal for %d is %x", 42, 589625));
+// 	printf(" - Count printf: %d\n", printf("Hexadecimal for %d is %x", 42, 589625));
+// }
