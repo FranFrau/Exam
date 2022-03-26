9027e387af47c845ecd1061c3bbe6f8064362569
FranFrau <francesco2001.ff@gmail.com>
2022-03-26 19:06:28 +0100

    printf exam update

--- Exam Rank 02/ft_printf.c
+++ Exam Rank 02/ft_printf.c
@@ -6,92 +6,100 @@
 /*   By: ffrau <ffrau@student.42roma.it>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 15:34:13 by ffrau             #+#    #+#             */
-/*   Updated: 2022/03/02 12:58:33 by ffrau            ###   ########.fr       */
+/*   Updated: 2022/03/26 19:04:38 by ffrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int	ft_putchar(char c)
+int	ft_putstr(char *str)
 {
-	return (write(1, &c, 1));
-}
-
-int	ft_putstr(char *c)
-{
-	int	count;
-
-	if (!c)
-		return (ft_putstr("(null)"));
-	count = 0;
-	while (c[count])
-		count += ft_putchar(c[count]);
-	return (count);
-}
-
-int	ft_atoi(long long nb, char *base, int nb_base)
-{
-	int	count;
-
-	count = 0;
-	if (nb == -2147483648 && nb_base == 10)
-		return (ft_putstr("-2147483648"));
-	else if (nb < 0)
+	int i = 0;
+	while (str[i])
 	{
-		if (nb_base != 16)
-			count += ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb < 10)
-		return (count += ft_putchar('0' + nb));
-	if (nb / nb_base > 0)
-		count += ft_atoi(nb / nb_base, base, nb_base);
-	count += ft_putchar(base[nb % nb_base]);
-	return (count);
-}
-
-int	ft_printf(const char *s, ...)
-{
-	va_list	list;
-	int		i;
-	int		count;
-
-	if (!s)
-		return (0);
-	i = 0;
-	count = 0;
-	va_start(list, s);
-	while (s[i])
-	{
-		if (s[i] == '%')
-		{
-			i++;
-			if (s[i] == 'd')
-				count += ft_atoi(va_arg(list, int), "0123456789", 10);
-			else if (s[i] == 's')
-				count += ft_putstr(va_arg(list, char *)));
-			else if (s[i] == 'x')
-				count += ft_atoi(va_arg(list, unsigned int), "0123456789abcdef", 16);
-			i++;
-		}
-		if (!s[i])
-			return (count);
-		count += ft_putchar(s[i]);
+		write (1, &str[i], 1);
 		i++;
 	}
-	return (count);
+	return (i);
 }
 
-// #include <stdio.h> 
-// int main()
-// {
-// 	printf(" - Count ft_printf: %d\n", ft_printf("Hello %s", "toto"));
-// 	printf(" - Count printf: %d\n", printf("Hello %s", "toto"));
-// 	printf(" - Count ft_printf: %d\n", ft_printf("%s %s", "I'm"));
-// 	printf(" - Count printf: %d\n", printf("%s %s", "I'm"));
-// 	printf(" - Count ft_printf: %d\n", ft_printf("Magic %s is %d", "number", 42));
-// 	printf(" - Count printf: %d\n", printf("Magic %s is %d", "number", 42));
-// 	printf(" - Count ft_printf: %d\n", ft_printf("Hexadecimal for %d is %x", 42, 589625));
-// 	printf(" - Count printf: %d\n", printf("Hexadecimal for %d is %x", 42, 589625));
-// }
+void ft_putnbr_base(long long num,  char *base, int base_len)
+{	
+	if (num < base_len)
+		write(1, &base[num], 1);
+	else
+	{
+		ft_putnbr_base(num / base_len, base, base_len);
+		ft_putnbr_base(num % base_len, base, base_len);
+	}
+}
+
+int	ft_print_d(long long num)
+{
+	int len = 1;
+	int mod = 1;
+	if (num < 0)
+	{
+		len++;
+		write (1, "-", 1);
+		num *= -1;
+	}
+	ft_putnbr_base(num, "0123456789", 10);
+	while (num / mod >= 10)
+	{
+		len++;
+		mod *= 10;
+	}
+	return (len);
+}
+
+int	ft_print_x(unsigned int num)
+{
+	int len = 1;
+	int mod = 1;
+	ft_putnbr_base(num, "0123456789abcdef", 16);
+	while (num / mod >= 16)
+	{
+		len++;
+		mod *= 16;
+	}
+	return (len);
+}
+
+int ft_printf(char *str, ...)
+{
+	va_list arg;
+	int	i = 0;
+	int	ret = 0;
+
+	va_start(arg, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			if (str[i] == 'd')
+				ret +=	ft_print_d(va_arg(arg, int));
+			else if (str[i] == 'x')
+				ret +=	ft_print_x(va_arg(arg, unsigned int));
+			else if (str[i] == 's')
+				ret += ft_putstr(va_arg(arg, char *));
+		}
+		else
+			ret += write (1, &str[i], 1);
+		i++;
+	}
+	va_end(arg);
+	return (ret);
+}
+/*
+int main ()
+{
+	int len;
+	int len2;
+
+	len = ft_printf("S: %s D: %d X: %x\n", "ciao", -10, 42420);
+	len2= printf("S: %s D: %d X: %x\n", "ciao", -10, 42420);
+	printf("MIO: %d - VERO:  %d", len, len2);
+}*/
